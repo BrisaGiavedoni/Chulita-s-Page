e869cbac7829e8b31f6a2e43e6eacebd924a5746
BrisaGiavedoni <giavedonibrisa@gmail.com>
2026-05-24 09:51:09 -0300

    feat: implement Cart context and components for managing shopping cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
+import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar/Navbar";
-
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
 
 function App() {
-
+  const [showCart, setShowCart] = useState(false);
 
   return (
-    <>
-     <Navbar />
-      
-    </>
-  )
+    <CartProvider>
+      <Navbar onCartClick={() => setShowCart(!showCart)} />
+      {showCart && <Cart />}
+      <Products />
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
